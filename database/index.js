4f99aa4586f46cd1bd59a4b0afa103e8cb9cb74f
Quentin <quentindevial19@gmail.com>
2024-03-07 14:03:19 +0100

    update: packages

--- database/index.js
+++ database/index.js
@@ -1,7 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 exports.clientPromise = mongoose
   .connect(
-    'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true'
+    "mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true"
   )
+  .then((m) => {
+    console.log("connexion db ok !");
+    return m;
+  })
   .catch((err) => console.log(err));
